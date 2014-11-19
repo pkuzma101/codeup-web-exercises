9fb3d9f51e4580a21119de68f9e7e6db01b00c7c
pkuzma101 <kuzma.paul@gmail.com>
2014-11-19 14:46:23 -0600

    First draft of Exercise 3.5.4 - two more if statements needed

---
+++ public/national_parks.php
@@ -0,0 +1,56 @@
+<?php
+
+define ('DB_HOST', '127.0.0.1');
+define ('DB_NAME', 'national_parks_db');
+define ('DB_USER', 'np_user');
+define ('DB_PASS', '');
+
+require '../inc/db_connect.php';
+
+if(isset($_GET['page'])) {
+	$pageNumber = $_GET['page'];
+} else {
+	$pageNumber = 1;
+}
+	
+$offsetNumber = ($pageNumber - 1) * 4;
+
+$parks = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET $offsetNumber")->fetchAll(PDO::FETCH_ASSOC);
+
+// $parksSecondPage = $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET 4')->fetchAll(PDO::FETCH_ASSOC);
+
+$rowCount = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
+
+
+?>
+
+<html>
+	<head>
+		<title>National Parks Bonanza</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	</head>
+	<body>
+		<h1>Parks</h1>
+		<table class="table table-striped">
+
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Location</th>
+				<th>Date Established</th>
+				<th>Area in Acres</th>
+			</tr>
+			<? foreach($parks as $key => $park): ?>
+			<tr>
+				<? foreach($park as $attribute): ?>
+				<td><?= htmlspecialchars(strip_tags($attribute)); ?></td>
+				<? endforeach ?>
+			<? endforeach ?>
+			</tr>
+		</table>
+		<div>
+			<a href="?page=<?= $pageNumber - 1 ?>" class='btn btn-info'> < Previous </a>
+			<a href="?page=<?= $pageNumber + 1 ?>" class='btn btn-danger'>Next ></a>
+		</div>
+	</body>
+</html>
