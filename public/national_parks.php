6e3fbb0d2e3eec646931b5927df120d86083a6db
pkuzma101 <kuzma.paul@gmail.com>
2014-11-20 14:23:43 -0600

    Exercise 9.3.4

--- public/national_parks.php
+++ public/national_parks.php
@@ -15,33 +15,51 @@ if(isset($_GET['page'])) {
 	
 $offsetNumber = ($pageNumber - 1) * 4;
 
-$parks = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET $offsetNumber")->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $dbc->prepare("SELECT * FROM national_parks LIMIT 4 OFFSET $offsetNumber");
+$stmt->bindValue(':offsetNumber', $offsetNumber, PDO::PARAM_INT);
+$stmt->execute();
 
-// $parksSecondPage = $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET 4')->fetchAll(PDO::FETCH_ASSOC);
+$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $numberOfParks = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
 
-// if($offsetNumber > $numberOfParks) {
+if($_POST) {
+	if(empty($_POST['name']) || empty($_POST['location']) || empty($_POST['date_established']) || empty($_POST['area_in_acres']) || empty($_POST['description'])) {
+		 echo "<div class='alert alert-info' role='alert'> Fill in all fields </div>";
+	} else {
+		if(isset($_POST['name']) && strlen($_POST['description'] < 125)) {
+			$query = $dbc->prepare('INSERT INTO national_parks(name, location, date_established, area_in_acres, description) VALUES(:name, :location, :date_established, :area_in_acres, :description)');
+				$query->bindValue(':name', $_POST['name'], PDO:: PARAM_STR);
+				$query->bindValue(':location', $_POST['location'], PDO:: PARAM_STR);
+				$query->bindValue(':date_established', $_POST['date_established'], PDO:: PARAM_STR);
+				$query->bindValue(':area_in_acres', $_POST['area_in_acres'], PDO:: PARAM_STR);
+				$query->bindValue(':description', $_POST['description'], PDO:: PARAM_STR);
 
-// }
-
+				$query->execute();
+		}
+	}
+}
 ?>
 
 <html>
 	<head>
 		<title>National Parks Bonanza</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Raleway|Nunito' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Smokum|Raleway|Nunito' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/national_parks.css">
 	</head>
 	<body>
-		<h1>Parks</h1>
-		<table class="table table-striped">
-
+		<h1>National Parks Bonanza</h1>
+		<table class="table table">
 			<tr>
 				<th>ID</th>
 				<th>Name</th>
 				<th>Location</th>
 				<th>Date Established</th>
 				<th>Area in Acres</th>
+				<th>Description</th>
 			</tr>
 			<? foreach($parks as $key => $park): ?>
 			<tr>
@@ -55,9 +73,37 @@ $numberOfParks = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn
 			<? if($pageNumber > 1): ?>
 			<a href="?page=<?= $pageNumber - 1 ?>" class='btn btn-info' id="previous"> < Previous </a>
 			<? endif ?>
-			<? if($pageNumber <= 2): ?>
+			<? if(round($numberOfParks / 4) > $pageNumber): ?>
 			<a href="?page=<?= $pageNumber + 1 ?>" class='btn btn-danger' id="next">Next ></a>
 			<? endif ?>
 		</div>
+		<!-- Form that creates a new park -->
+		<form method="POST" classaction="/national_parks.php" class='form-horizontal' role='form'>
+			<h2>Insert a New Park</h2>
+			<p class='input-group'>
+				<label for="name">Name:</label>
+				<input id="park" name="name" type="text" placeholder="Name">
+			</p>
+			<p class='input-group'>
+				<label for="location">Location:</label>
+				<input id="park" name="location" type="text" placeholder="Location">
+			</p>
+			<p class='input-group'>
+				<label for="date_established">Date Established:</label>
+				<input id="park" name="date_established" type="text" placeholder="yyyy-mm-dd">
+			</p>
+			<p class='input-group'>
+				<label for="area_in_acres">Area(acres):</label>
+				<input id="park" name="area_in_acres" type="text" placeholder="Area(acres)">
+			</p>
+			<p class='input-group'>
+				<label for="description">Description:</label>
+				<input id="park" name="description" type="text" placeholder="125 characters or less">
+			</p>
+			<p>
+				<!-- Creates button used to add new park info -->
+				<button type="submit" class="btn btn-primary">Add Park</button>
+			</p>
+		</form>
 	</body>
 </html>
